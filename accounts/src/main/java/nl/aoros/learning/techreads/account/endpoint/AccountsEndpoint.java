ac84b4e3b3213c25931de6a0a2cc45482f15276a
Adrian Oros <lucian.adrian.oros@gmail.com>
2018-05-20 20:19:31 +0200

    removed swagger as not compatible yet with boot2 reactive web

--- accounts/src/main/java/nl/aoros/learning/techreads/account/endpoint/AccountsEndpoint.java
+++ accounts/src/main/java/nl/aoros/learning/techreads/account/endpoint/AccountsEndpoint.java
@@ -1,9 +1,5 @@
 package nl.aoros.learning.techreads.account.endpoint;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import nl.aoros.learning.techreads.account.endpoint.dto.AccountDTO;
 import nl.aoros.learning.techreads.account.endpoint.dto.AccountMapper;
 import nl.aoros.learning.techreads.account.service.AccountService;
@@ -19,10 +15,6 @@ import reactor.core.publisher.Mono;
  */
 @RestController
 @RequestMapping("/api")
-@Api("Accounts web service endpoint")
-@ApiResponses({
-        @ApiResponse(code = 500, message = "Internal service exception"),
-        @ApiResponse(code = 404, message = "The account does not exist")})
 public class AccountsEndpoint {
 
     private AccountService accountService;
@@ -37,42 +29,30 @@ public class AccountsEndpoint {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    @ApiOperation("Create a new account")
-    @ApiResponses({
-            @ApiResponse(code = 201, message = "The account was created", response = String.class),
-            @ApiResponse(code = 400, message = "The account already exists")})
     public Mono<String> createAccount(@RequestBody @Validated(AccountDTO.Create.class) AccountDTO account) {
         return Mono.just(account).map(mapper::fromDto).flatMap(accountService::addAccount);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation("Delete an account")
-    @ApiResponses(@ApiResponse(code = 200, message = "The account was removed"))
     public Mono<Void> deleteAccount(@PathVariable String id) {
         return accountService.deleteAccount(id);
     }
 
     @PutMapping
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation("Update an account")
-    @ApiResponses(@ApiResponse(code = 200, message = "The account was updated"))
     public Mono<Void> updateAccount(@RequestBody @Validated(AccountDTO.Update.class) AccountDTO accountDTO) {
         return Mono.just(accountDTO).map(mapper::fromDto).flatMap(accountService::updateAccount);
     }
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation("Retrieve the account details")
-    @ApiResponses(@ApiResponse(code = 200, message = "The account was found and present in the response"))
     public Mono<AccountDTO> getAccount(@PathVariable String id) {
         return accountService.getAccount(id).map(mapper::toDto);
     }
 
     @GetMapping("/all")
     @ResponseStatus(HttpStatus.OK)
-    @ApiOperation("Retrieves all the account")
-    @ApiResponses(@ApiResponse(code = 200, message = "The list of accounts is present in the response"))
     public Flux<AccountDTO> getAccounts() {
         return accountService.findAllAccounts().map(mapper::toDto);
     }
--- accounts/src/main/java/nl/aoros/learning/techreads/account/endpoint/dto/AccountDTO.java
+++ accounts/src/main/java/nl/aoros/learning/techreads/account/endpoint/dto/AccountDTO.java
@@ -1,6 +1,5 @@
 package nl.aoros.learning.techreads.account.endpoint.dto;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,30 +13,23 @@ import java.time.LocalDateTime;
 @Data
 @NoArgsConstructor
 public class AccountDTO {
-    @ApiModelProperty(value = "The account id")
     @NotBlank(message = "The id is required", groups = Update.class)
     private String id;
 
-    @ApiModelProperty(value = "The account name")
     @NotBlank(message = "The name is required", groups = {Update.class, Create.class})
     private String name;
 
-    @ApiModelProperty(value = "Whether the account is enabled or not")
     @NotNull(message = "The enabled flag is required", groups = {Update.class, Create.class})
     private Boolean enabled;
 
-    @ApiModelProperty(value = "The account email address")
     @NotBlank(message = "The email is required", groups = {Update.class, Create.class})
     private String email;
 
-    @ApiModelProperty(value = "The account location")
     @NotBlank(message = "The location is required", groups = {Update.class, Create.class})
     private String location;
 
-    @ApiModelProperty(value = "The account create date")
     private LocalDateTime createDate;
 
-    @ApiModelProperty(value = "The password change date")
     private LocalDateTime lastPasswordChange;
 
     public interface Create {
